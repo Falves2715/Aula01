f4025e46dedff9634288a3ea6951fc9a335b294b
Felipe <falves2715@gmail.com>
2023-08-08 19:10:15 -0300

    Commit Pycharm

---
+++ opa.py
@@ -0,0 +1,8 @@
+import numpy
+
+ndarray1 = numpy.zeros(100000)
+print(f' 1- Conteudo da Lista: {ndarray1}, o tamanho : {len(ndarray1)}')
+ndarray1 = numpy.ones(100000)
+print(f' 1- Conteudo da Lista: {ndarray1}, o tamanho : {len(ndarray1)}')
+ndarray1 = numpy.linspace(10, 1000, 1000)
+print(f' 1- Conteudo da Lista: {ndarray1}, o tamanho : {len(ndarray1)}')
